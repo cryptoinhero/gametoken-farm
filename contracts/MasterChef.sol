7c74813d4d8c8564f77294a2e2b8d947171f78df
risingstar2018 <33366277+risingstar2018@users.noreply.github.com>
2021-04-16 08:17:08 +0800

    Update MasterChef.sol

--- contracts/MasterChef.sol
+++ contracts/MasterChef.sol
@@ -170,22 +170,22 @@ contract MasterChef is Ownable {
         }
     }
 
-    // Set the migrator contract. Can only be called by the owner.
-    function setMigrator(IMigratorChef _migrator) public onlyOwner {
-        migrator = _migrator;
-    }
+    // // Set the migrator contract. Can only be called by the owner.
+    // function setMigrator(IMigratorChef _migrator) public onlyOwner {
+    //     migrator = _migrator;
+    // }
 
-    // Migrate lp token to another lp contract. Can be called by anyone. We trust that migrator contract is good.
-    function migrate(uint256 _pid) public {
-        require(address(migrator) != address(0), "migrate: no migrator");
-        PoolInfo storage pool = poolInfo[_pid];
-        IBEP20 lpToken = pool.lpToken;
-        uint256 bal = lpToken.balanceOf(address(this));
-        lpToken.safeApprove(address(migrator), bal);
-        IBEP20 newLpToken = migrator.migrate(lpToken);
-        require(bal == newLpToken.balanceOf(address(this)), "migrate: bad");
-        pool.lpToken = newLpToken;
-    }
+    // // Migrate lp token to another lp contract. Can be called by anyone. We trust that migrator contract is good.
+    // function migrate(uint256 _pid) public {
+    //     require(address(migrator) != address(0), "migrate: no migrator");
+    //     PoolInfo storage pool = poolInfo[_pid];
+    //     IBEP20 lpToken = pool.lpToken;
+    //     uint256 bal = lpToken.balanceOf(address(this));
+    //     lpToken.safeApprove(address(migrator), bal);
+    //     IBEP20 newLpToken = migrator.migrate(lpToken);
+    //     require(bal == newLpToken.balanceOf(address(this)), "migrate: bad");
+    //     pool.lpToken = newLpToken;
+    // }
 
     // Return reward multiplier over the given _from to _to block.
     function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
